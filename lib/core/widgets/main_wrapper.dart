d6063bc0b08580749b16c412769bf37cef9b19df
majid <majid.gha3mi.p@gmail.com>
2025-06-29 16:43:43 +0330

    sccafold created

--- lib/core/widgets/main_wrapper.dart
+++ lib/core/widgets/main_wrapper.dart
@@ -5,6 +5,9 @@ class MainWrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
   }
 }
