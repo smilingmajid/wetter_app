84793d3fe33bc7bfa2d34b6c70e044e827b95db2
majid <majid.gha3mi.p@gmail.com>
2025-06-29 17:13:04 +0330

    comment

--- lib/features/feature_weather/data/data_source/remote/api_provider.dart
+++ lib/features/feature_weather/data/data_source/remote/api_provider.dart
@@ -2,18 +2,22 @@ import 'package:dio/dio.dart';
 import 'package:wetter_app/core/utils/constants.dart';
 
 class ApiProvider {
-  
+
  final Dio _dio = Dio();
  var apiKey=Constants.apiKey;
  var baseUrl=Constants.baseUrl;
 
- callCurrentWeather(cityName){
-  var response = _dio.get("$baseUrl/data/2.5/weather",
+
+  /// Fetches the current weather data for a given city name.
+
+ Future<dynamic> callCurrentWeather(cityName) async {
+  var response = await _dio.get("$baseUrl/data/2.5/weather",
       queryParameters: {
         "q": cityName,
         "appid": apiKey,
         "units": "metric"
       });
+      print(response.data);
   return response;
   
  }
